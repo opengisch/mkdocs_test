aad5bbf45c1020d67d1910e091c87ff70fa0bcbd
Matthias Kuhn <matthias@opengis.ch>
2020-12-15 09:04:44 +0100

    Add config option to make preview width configurable

--- extensions/fancyboxmd/fancyboxmd.py
+++ extensions/fancyboxmd/fancyboxmd.py
@@ -6,6 +6,10 @@ import xml.etree.ElementTree as etree
 class FancyBoxInlineProcessor(LinkInlineProcessor):
     """ Return a img element with fancybox attributes from the given match. """
 
+    def __init__(self, re, preview_width, md):
+        self.preview_width = preview_width
+        super().__init__(re, preview_width, md)
+
     def handleMatch(self, m, data):
         text, index, handled = self.getText(data, m.end(0))
         if not handled:
@@ -23,6 +27,7 @@ class FancyBoxInlineProcessor(LinkInlineProcessor):
 
         el_img = etree.Element("img")
         el_img.set("src", src)
+        el_img.set("width", self.preview_width)
 
         if title is not None:
             el_img.set("title", title)
@@ -41,11 +46,16 @@ class FancyBoxExtension(Extension):
     """
 
     def __init__(self, **kwargs):
+        # Define config options and defaults
+        self.config = {
+            "preview_width": ['600px', "Default width of the preview pictures."],
+        }
         # Call the parent class's __init__ method to configure options
         super().__init__(**kwargs)
 
     def extendMarkdown(self, md):
-        pattern = FancyBoxInlineProcessor(FB_BRACKETS_RE, md)
+        preview_width = self.getConfig("preview_width"):
+        pattern = FancyBoxInlineProcessor(FB_BRACKETS_RE, preview_width, md)
         md.inlinePatterns.register(pattern, "fancybox-brackets", 200)
 
 
